dd5eb52a9f32a843c8021f7a0735983cfdb9f6d4
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-05-18 16:10:44 +0100

    fix

--- src/actions/create_entry.ts
+++ src/actions/create_entry.ts
@@ -1,18 +1,1 @@
 "use server"
-
-type Player = {
-    rating: number,
-    player: string
-}
-
-enum Success {
-    Success,
-    Fail
-}
-
-const async create_entry: Array<Player> = (username: string) => {
-    const await fetch('')
-    return () 
-}
-
-export create_entry
