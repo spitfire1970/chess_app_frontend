51b285c53caefd70dc9aac49f85b8f1fab394367
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-05-20 23:37:03 +0100

    resolve https vs http issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_DEPLOYED
+
+
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+	async rewrites() {
+		return [
+			{
+				source: '/api/proxy/:path*',
+				destination: `${API_URL}/:path*`,
+			},
+		]
+	},
 };
 
 export default nextConfig;
--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -1,48 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
+import httpProxy from 'http-proxy'
+import { NextApiRequest, NextApiResponse } from 'next/types'
 
-const api = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_DEPLOYED
+const API_URL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_DEPLOYED
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { path: string[] } }
-): Promise<Response> {
-  params = await params
-  const path = params.path.join('/');
-  const url = new URL(request.url);
-  const apiUrl = `${api}/${path}${url.search}`;
+const proxy = httpProxy.createProxyServer()
 
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error('API proxy error:', error);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
-  }
+export const config = {
+	api: {
+		bodyParser: false,
+	},
 }
 
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { path: string[] } }
-): Promise<Response> {
-  params = await params
-  const path = params.path.join('/');
-  const body = await request.json();
-  const apiUrl = `${api}/${path}`;
-
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    
-    const data = await response.json();
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error('API proxy error:', error);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
-  }
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise<void>((resolve, reject) => {
+		proxy.web(req, res, { target: API_URL, changeOrigin: true }, (err) => {
+			if (err) {
+				return reject(err)
+			}
+			resolve()
+		})
+	})
 }
