6cf2e0292a776c7c99ece4d0ba294d79a3075b3a
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-05-28 01:01:16 +0100

    wake logic

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_DEPLOYED
-
+const GPU_URL = process.env.NEXT_PUBLIC_GPU_URL_DEPLOYED
 
 const nextConfig: NextConfig = {
   eslint: {
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
 				source: '/api/proxy/:path*',
 				destination: `${API_URL}/:path*`,
 			},
+    {
+      source: '/gpu/proxy',
+      destination: `${GPU_URL}`, // <-- for root
+    },
+    {
+      source: '/gpu/proxy/:path*',
+      destination: `${GPU_URL}/:path*`, // <-- for paths
+    }
 		]
 	},
 };
