3deaf1cffb49eebc988da3820cebd0dd7ad46a22
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-06-09 01:36:17 +0100

    sep

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import "./globals.css";
+import { ReactNode } from "react";
 
-const Layout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {return (<html>
-<head>
-  
-</head>
-  <body suppressHydrationWarning className = "bg-black">
-{children}    
-  </body>
-</html>)
-}
+export const metadata = {
+  title: "Nakul's personalized chess app",
+  description: "Play with personalized chess bots that mimic the play style of different chess.com players using ML",
+  icons: {
+    icon: "https://utfs.io/f/your-file.png",
+  },
+};
 
-export default Layout;
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning className="bg-black">
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
